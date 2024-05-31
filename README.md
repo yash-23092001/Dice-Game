7ee3362f382505e606b0eeb184bd9568de6f773d
yash-23092001 <yash.chhelaiya@gmail.com>
2024-05-31 18:42:21 -0400

    Dice-Game

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # Dice-Game
+a quick way to decide a winner in two peoples argument.
---
+++ index.js
@@ -0,0 +1,17 @@
+var randomNumber1 = Math.floor(Math.random() * 5) + 1;
+var randomNumber2 = Math.floor(Math.random() * 5) + 1;
+
+var randomImage1 = "images/dice" + randomNumber1 + ".png";
+var randomImage2 = "images/dice" + randomNumber2 + ".png";
+
+document.getElementsByClassName("img1")[0].setAttribute("src", randomImage1);
+document.getElementsByClassName("img2")[0].setAttribute("src", randomImage2);
+
+var result = document.querySelector(".container h1")
+if (randomNumber1 > randomNumber2) {
+    result.innerHTML = "Player 1 Wins !";
+} else if (randomNumber1 < randomNumber2) {
+    result.innerHTML = "Player 2 Wins !";
+} else {
+    result.innerHTML = "Draw !";
+}
