ecd40c43001820a43ba1f6a88d16e4ba4f409628
yash-23092001 <yash.chhelaiya@gmail.com>
2024-05-31 18:47:05 -0400

    Dice-Game

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-var randomNumber1 = Math.floor(Math.random() * 5) + 1;
-var randomNumber2 = Math.floor(Math.random() * 5) + 1;
+var randomNumber1 = Math.floor(Math.random() * 6) + 1;
+var randomNumber2 = Math.floor(Math.random() * 6) + 1;
 
 var randomImage1 = "images/dice" + randomNumber1 + ".png";
 var randomImage2 = "images/dice" + randomNumber2 + ".png";
